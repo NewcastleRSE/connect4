2f3fbe20e643a7dd5d809748c828ad8613f2edc9
Robin Nandi <robin.nandi@newcastle.ac.uk>
2023-08-25 12:56:20 +0100

    added classic pettingzoo environments

---
+++ rock_paper_scissors.py
@@ -0,0 +1,18 @@
+from pettingzoo.classic import rps_v2
+import time
+
+env = rps_v2.env(render_mode="human")
+env.reset(seed=42)
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+
+    if termination or truncation:
+        action = None
+    else:
+        # this is where you would insert your policy
+        action = env.action_space(agent).sample()
+        time.sleep(1)
+
+    env.step(action)
+env.close()
